48099970e85246efba58758177bff039b9356f99
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2025-04-27 14:44:35 -0500

    se programa la barra de busqueda para encontrar productos e insumos y cuando se selecciona, que aparezca en la tabla principal. Se puede editar la cantidad a modificar y se modifica para que no supere su max y min que programaste en el inventario

--- src/router/links/inventory.js
+++ src/router/links/inventory.js
@@ -17,6 +17,10 @@ const {
 } = require('../../services/connectionWithDatabaseImage');
 const { get } = require('node-persist');
 
+const {
+    get_inventory_products_branch,
+    get_inventory_supplies_branch
+} = require('../../services/supplies');
 
 const XLSX = require('xlsx');
 const path = require('path');
@@ -28,46 +32,13 @@ router.get('/:id_company/:id_branch/mass-product-adjustment', isLoggedIn, async 
     res.render('links/free/inventory/updateInventory.hbs',{branchFree});
 })
 
-async function get_prescription_details(dateStart, dateFinish) {
-    if (dateStart && !dateStart.includes(' ')) {
-        dateStart = `${dateStart} 00:00:00`;
-    }
-
-    // Si solo se pasa la fecha sin hora, asignamos la hora 23:59:59 para dateFinish
-    if (dateFinish && !dateFinish.includes(' ')) {
-        dateFinish = `${dateFinish} 23:59:59`;
-    }
-    const queryText = `
-        SELECT 
-            p.id AS prescription_id,
-            p.recipe_folio,
-            p.doctor_id,
-            p.doctor_name,
-            p.date_prescription,
-            p.retained,
-            p.amount,
-            p.comment,
-            l.number_lote,
-            l.expiration_date,
-            dc.barcode,
-            dc.name AS product_name,
-            dc.description AS product_description
-        FROM "Branch".prescription p
-        LEFT JOIN "Inventory".lots l ON p.id_lots = l.id
-        LEFT JOIN "Inventory".dish_and_combo_features dcf ON l.id_dish_and_combo_features = dcf.id
-        LEFT JOIN "Kitchen".dishes_and_combos dc ON dcf.id_dishes_and_combos = dc.id
-        WHERE dc.this_product_need_recipe = true
-        AND p.date_prescription BETWEEN $1 AND $2;  -- Filtro por rango de fechas
-    `;
-
-    try {
-        // Realizar la consulta pasando las fechas como parámetros
-        const result = await database.query(queryText, [dateStart, dateFinish]);
-        return result.rows;
-    } catch (error) {
-        console.error('Error getting prescription details:', error);
-        return [];
-    }
-}
+router.post('/:id_branch/search_products_for_update_the_inventory', isLoggedIn, async (req, res) => {
+    const {id_branch } = req.params;
+    const {barcode}=req.body;
+    const products = await get_inventory_products_branch(id_branch,barcode);
+    const supplies = await get_inventory_supplies_branch(id_branch,barcode);
+    console.log(products, supplies);
+    res.json({ products, supplies });
+});
 
 module.exports = router;
