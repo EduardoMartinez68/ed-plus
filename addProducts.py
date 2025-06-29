fcc1b0fb74bcf643d96d907fe8c4f358efc5e9e7
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2025-06-28 21:20:19 -0500

    se cambia la base de datos a la nueva que no tiene errores al momento de crear productos para sqlite

---
+++ addProducts.py
@@ -0,0 +1,10 @@
+#voy a ver el indice final que tiene mi base de datos de productos en mysql para editar todos los productos despues de este id
+
+ultimpId=25
+
+#primero se agregaran a mi base de datos en mysql
+
+#despues se buscara con ayuda de BARCODE todos los productos que vienen en mi base de datos mysql a partir del ultimpId, si ira leyendo por cada 2 segundos un nuevo bardoe. 
+# Esto solo se hara por 100 productos. Se guardara el ultimo iD leido en mi base de datos para continuar despues. 
+
+#si encontro relacion, obtendra la imagen de la base de datos y se actualizara con su enlace
