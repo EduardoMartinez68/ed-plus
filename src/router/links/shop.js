bfe4d8630589f433bbe75c50d2bddce8a10972fa
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2025-07-07 14:21:58 -0500

    ya se pueden ver el catalogo de los productos y agregar productos desde el software para no ir hasta la web

--- src/router/links/shop.js
+++ src/router/links/shop.js
@@ -21,18 +21,6 @@ const {
     get_all_box_of_the_branch_with_his_id
 } = require('../../services/branch');
 
-const {
-    get_data_employee
-} = require('../../services/employees');
-
-const {
-    get_all_invoice_with_the_id_of_the_branch
-} = require('../../services/invoice');
-
-const {
-    get_data_company_with_id
-} = require('../../services/company');
-
 //functions permission
 const {
     this_user_have_this_permission
@@ -61,67 +49,46 @@ router.get('/:id_company/:id_branch/shop', isLoggedIn, async (req, res) => {
 });
 
 
-async function get_the_information_of_the_ticket(ticketCode) {
-    try {
-        let query;
-        let values;
-        let result;
-
-        if (!ticketCode) {
-            return { success: false, error: "Código de ticket requerido." };
+router.get('/:id_company/:id_branch/products-shop', isLoggedIn, async (req, res) => {
+    const { id_company, id_branch } = req.params;
+    //fitst we will see if exist a token in this desktop
+    const token=getToken();
+    if(token){
+        //we will see if the user not have the permission for this App.
+        if (!this_user_have_this_permission(req.user, id_company, id_branch, 'edit_shop_online')) {
+            req.flash('message', 'Lo siento, no tienes permiso para esta acción 😅');
+            return res.redirect(`/links/${id_company}/${id_branch}/permission_denied`);
         }
 
-        if (TYPE_DATABASE === 'sqlite') {
-            query = `
-                SELECT 
-                    t.*,
-                    hr.id AS history_return_id,
-                    hr.old_ticket,
-                    hr.date_return,
-                    hr.total_return,
-                    hr.products_returns,
-                    hr.note AS return_note
-                FROM ticket t
-                LEFT JOIN history_returns hr ON hr.id_ticket = t.id
-                WHERE t.key = ?;
-            `;
 
-            result = await database.all(query, [ticketCode]);
-
-            return {
-                success: true,
-                message: "Información del ticket obtenida.",
-                data: result
-            };
-
-        } else {
-            query = `
-                SELECT 
-                    t.*,
-                    hr.id AS history_return_id,
-                    hr.old_ticket,
-                    hr.date_return,
-                    hr.total_return,
-                    hr.products_returns,
-                    hr.note AS return_note
-                FROM "Box".ticket t
-                LEFT JOIN "Box".history_returns hr ON hr.id_ticket = t.id
-                WHERE t.key = $1;
-            `;
-
-            const pgResult = await database.query(query, [ticketCode]);
-
-            return {
-                success: true,
-                message: "Información del ticket obtenida.",
-                data: pgResult.rows
-            };
-        }
-
-    } catch (error) {
-        console.error("Error al obtener información del ticket:", error);
-        return { success: false, error: "No se pudo obtener la información del ticket." };
+        const branchFree = await get_data_branch(id_branch);
+        res.render('links/shop/catalogo', {branchFree, token});
+    }else{
+        req.flash('message', 'Lo siento, no tienes ningun Token valido en este equipo 😅');
+        return res.redirect(`/links/${id_company}/${id_branch}/permission_denied`);  
     }
-}
+});
+
+router.get('/:id_company/:id_branch/add-products-shop', isLoggedIn, async (req, res) => {
+    const { id_company, id_branch } = req.params;
+    //fitst we will see if exist a token in this desktop
+    const token=getToken();
+    if(token){
+        //we will see if the user not have the permission for this App.
+        if (!this_user_have_this_permission(req.user, id_company, id_branch, 'edit_shop_online')) {
+            req.flash('message', 'Lo siento, no tienes permiso para esta acción 😅');
+            return res.redirect(`/links/${id_company}/${id_branch}/permission_denied`);
+        }
+
+
+        const branchFree = await get_data_branch(id_branch);
+        res.render('links/shop/addProduct', {branchFree, token});
+    }else{
+        req.flash('message', 'Lo siento, no tienes ningun Token valido en este equipo 😅');
+        return res.redirect(`/links/${id_company}/${id_branch}/permission_denied`);  
+    }
+});
+
+
 
 module.exports = router;
